721d80815c9d64a2a25d03c377d7de6a9070ae65
James M. Irving, Ph.D. <james.irving.phd@gmail.com>
2024-07-01 15:13:38 -0400

    Added API workflow and files

--- config/conda-create-command-bash.sh
+++ config/conda-create-command-bash.sh
@@ -1,26 +1,26 @@
-# #!/bin/bash
+#!/bin/bash
 
-# # Create a new conda environment named 'nlp_project' with Python 3.8
-# conda create -n nlp-env python=3.10 -y
+# Create a new conda environment named 'nlp_project' with Python 3.8
+conda create -n nlp-env python=3.10 -y
 
-# # Activate the new environment
-# source activate nlp-env
+# Activate the new environment
+source activate nlp-env
 
 
-# # Pip install custom package
-# pip install dojo-ds
+# Pip install custom package
+pip install dojo-ds
 
-# # Install the required packages
-# conda install -y pandas numpy matplotlib seaborn nltk gensim pyldavis jupyter scikit-learn
+# Install the required packages
+conda install -y pandas numpy matplotlib seaborn nltk gensim pyldavis jupyter scikit-learn
 
-# # Install the kenrel in jupyter
-# python -m ipykernel install --user --name=nlp-env --display-name="Python(nlp-env)"
+# Install the kenrel in jupyter
+python -m ipykernel install --user --name=nlp-env --display-name="Python(nlp-env)"
 
-# # Additional installations if required
-# # conda install -y <other-packages>
-# # pip install <other-pip-packages>
+# Additional installations if required
+# conda install -y <other-packages>
+# pip install <other-pip-packages>
 
-# # Deactivate the environment
-# conda deactivate
+# Deactivate the environment
+conda deactivate
 
-# echo "Environment 'nlp_project' created and packages installed successfully."
+echo "Environment 'nlp_project' created and packages installed successfully."
